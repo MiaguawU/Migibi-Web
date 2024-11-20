dbb3e1f20e6d8245178059e32f48047d571e1d6f
MiaguawU <isis.miaguaw@gamil.com>
2024-11-20 16:32:13 -0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Inicio from './Front/Inicio';
+import Perfil from './Front/Perfil';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+            <div>
+                <header style={{ padding: '20px', textAlign: 'center', backgroundColor: '#282c34', color: 'white' }}>
+                  
+                    <nav style={{ marginTop: '10px' }}>
+                        <Link to="/" style={{ margin: '0 15px', color: 'white', textDecoration: 'none' }}>Inicio</Link>
+                        <Link to="/perfil" style={{ margin: '0 15px', color: 'white', textDecoration: 'none' }}>Perfil</Link>
+                    </nav>
+                </header>
+                <main style={{ padding: '20px' }}>
+                    <Routes>
+                        <Route path="/" element={<Inicio />} />
+                        <Route path="/perfil" element={<Perfil />} />
+                    </Routes>
+                </main>
+            </div>
+        </Router>
   );
 }
 
