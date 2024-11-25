645712e05cdbb719bf0c256b0670e8ee5b59dffb
CarlosDan0 <bolanos.nunez.carlos.daniel@gmail.com>
2024-11-25 01:37:54 -0600

    Pull con Cam y primer commit de Carlos

--- src/Front/Plan2.tsx
+++ src/Front/Plan2.tsx
@@ -1,4 +1,18 @@
 
+import { Row, Card, Col } from 'antd';  
+import imgdesayuno from "../Img/imgdesayuno.png";
+import defRec from "../Img/defRec.png";
+import btEditar from "../Img/btEditar.png";
+import btCompartir from "../Img/btCompartir.png";
+import imgCal from "../Img/imgCal.png";
+import btEliminar from "../Img/btEliminar.png";
+import relojarena from "../Img/relojarena.png";
+import cuadros from "../Img/cuadros.png";
+import flechaizquierda from "../Img/flechaizquierda.png"; 
+import flechaderecha from "../Img/flechaderecha.png";
+import calendario from "../Img/calendario.png";
+import btagregar from "../Img/btagregar.png";
+
 export default function Inicio() {
     return (
       <>
