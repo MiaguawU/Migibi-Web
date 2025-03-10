f2e3a7a486f4d9a2e6fc0422dd0d955d18c28aac
MiaguawU <isis.miaguaw@gamil.com>
2025-03-09 21:25:27 -0600

    ahpra con port

--- src/Server/base/connection.js
+++ src/Server/base/connection.js
@@ -8,6 +8,8 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: { rejectUnauthorized: true }, // Requerido si Railway usa SSL
 });
 
 db.connect((err) => {
@@ -15,7 +17,7 @@ db.connect((err) => {
     console.error('Error al conectar con la base de datos:', err.message);
     process.exit(1);
   }
-  console.log('Se pudo conectar a la base');
+  console.log('✅ Conexión exitosa a la base de datos en Railway');
 });
 
 module.exports = db;
