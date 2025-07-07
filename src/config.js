a1dcb3fbb9d6e379e5f8aeeb31590e44a39f9933
MiaguawU <isis.miaguaw@gamil.com>
2025-07-07 10:22:06 -0600

    en teorria deberia funcionar puerto

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const PUERTO = "http://localhost:5000";
+const PUERTO = process.env.REACT_APP_SERVER_URL || "http://localhost:5000";
 console.log("El valor de PUERTO es:", PUERTO); // Para depuración
 export default PUERTO;
 
