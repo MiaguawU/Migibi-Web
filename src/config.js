f9b282a36299750950297d1f0ffa22c0b2d4579f
MiaguawU <isis.miaguaw@gamil.com>
2025-07-07 11:48:38 -0600

    ya dios pr favor, config.js pq no se que pasa

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const PUERTO = process.env.REACT_APP_SERVER_URL || "http://localhost:5000";
+const PUERTO = process.env.REACT_APP_SERVER_URL || "https://migibi-servidor.onrender.com";
 console.log("El valor de PUERTO es:", PUERTO); // Para depuración
 export default PUERTO;
 
